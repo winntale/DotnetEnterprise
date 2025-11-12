703f79453a66adf85b90c65bf2c2b865354b0d5a
winntale <wintale126@gmail.com>
2025-11-13 00:26:47 +0300

    added app which contains a method returning only an odd numbers from a collections. app covered by AAA tests using xUnit

---
+++ OddFilterTests/GetOddTests.cs
@@ -0,0 +1,70 @@
+using Xunit;
+
+namespace OddFilterTests;
+
+public class GetOddTests
+{
+    private readonly OddFilter.OddFilter _oddfilter = new();
+
+    [Fact]
+    public void GetOdd_ShouldThrowNullException_WhenNullInput()
+    {
+        // Arrange
+        ICollection<int> nums = null;
+        
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => _oddfilter.GetOdd(nums).ToList());
+    }
+    
+    [Fact]
+    public void GetOdd_ShouldReturnEmpty_WhenEmptyInput()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int>();
+        
+        // Act
+        var result = _oddfilter.GetOdd(nums).ToList();
+
+        // Assert
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void GetOdd_ShouldReturnEmpty_WhenAllEven()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int> { 2, 4, 6 };
+        
+        // Act
+        var result = _oddfilter.GetOdd(nums).ToList();
+        
+        // Assert
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void GetOdd_ShouldReturnAll_WhenAllOdd()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int> { 1, 3, 5 };
+        
+        // Act
+        var result = _oddfilter.GetOdd(nums).ToList();
+        
+        // Assert
+        Assert.Equal(new[] { 1, 3, 5 }, result);
+    }
+    
+    [Fact]
+    public void GetOdd_ShouldReturnOnlyOdd_WhenMixedInput()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        
+        // Act
+        var result = _oddfilter.GetOdd(nums).ToList();
+        
+        // Assert
+        Assert.Equal(new[] { 1, 3, 5, 7, 9 }, result);
+    }
+}
