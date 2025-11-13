413097cfa75aceceac95cad8685ab9aff8952967
winntale <wintale126@gmail.com>
2025-11-14 00:09:10 +0300

    added app which contains a method returning only a positive numbers from a collections. app covered by AAA tests using xUnit

---
+++ PositiveFilterApp/PositiveFilter.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace PositiveFilter;
+public class PositiveFilter
+{
+    public ICollection<int> GetPositiveNumbers(ICollection<int>? numbers)
+    {
+        if (numbers == null) return new List<int>();
+        return numbers.Where(n => n > 0).ToList();
+    }
+
+    public static void Main(string[] args)
+    {
+        ICollection<int> numbers = new List<int> { 1, -2, 3, -4, 2, -6, 4, -8, 1 };
+        PositiveFilter positiveFilter = new();
+        positiveFilter.GetPositiveNumbers(numbers);
+        WriteLine(string.Join(", ", positiveFilter.GetPositiveNumbers(numbers)));
+    }
+}
---
+++ PositiveFilterTestsApp/PositiveFilterTests.cs
@@ -0,0 +1,73 @@
+﻿using Xunit;
+
+namespace PositiveFilterTests;
+
+public class PositiveFilterTests
+{
+    private readonly PositiveFilter.PositiveFilter _filter = new();
+    
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnEmpty_WhenNullInput()
+    {
+        // Arrange
+        ICollection<int>? nums = null;
+        
+        // Act
+        var result = _filter.GetPositiveNumbers(nums);
+        
+        // Assert
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnEmpty_WhenEmptyInput()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int>();
+        
+        // Act
+        var result = _filter.GetPositiveNumbers(nums);
+        
+        // Assert
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnEmpty_WhenNegativeInput()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int> { -1, -3, -5, -12, -1, -1 };
+        
+        // Act
+        var result = _filter.GetPositiveNumbers(nums);
+        
+        // Assert
+        Assert.Empty(result);
+    }
+    
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnAll_WhenPositiveInput()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int> { 1, 2, 3 };
+        
+        // Act
+        var result = _filter.GetPositiveNumbers(nums);
+        
+        // Assert
+        Assert.Equal(new List<int> { 1, 2, 3 }, result);
+    }
+
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnPositive_WhenMixedInput()
+    {
+        // Arrange
+        ICollection<int> nums = new List<int> { -3, 1, -5, 2, 3, 10, -1, 0, 0, 1, 5, 2 };
+        
+        // Act
+        var result = _filter.GetPositiveNumbers(nums);
+        
+        // Assert
+        Assert.Equal(new List<int> { 1, 2, 3, 10, 1, 5, 2 }, result);
+    }
+}
